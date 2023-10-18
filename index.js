a9b5bb404b061b1c881a90bda3e0adbfd6e81970
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-10-18 16:01:18 +0300

    counter set to 0 after save button has been clicked

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let count = 0;
 function incrementBtn() {
     console.log('btn was clicked');
     count += 1;
-    countElement.innerText = count;
+    // countElement.innerText = count;
+    countElement.textContent = count;
 }
 // incrementBtn(); - do not invoke function here!!! it has been already invoked in html code in button!
 
@@ -15,7 +16,9 @@ function save() {
 
     let saveMessage = count + ' ' + separator + ' ';
 
-    saveElement.textContent += saveMessage; //textContent is an alternative to innerText, but renders hidden symbols like spaces etc.
+    saveElement.textContent += saveMessage; //textContent is an alternative to innerText, but renders hidden symbols like spaces etc.Also innerText trigger the reflow every tiem to check new styles appears so it is better to use textContent  instead
 
     console.log(count);
+    count = 0;
+    countElement.textContent=count;
 }
