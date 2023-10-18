ca1a3e20149b92de12c342e93f16bc47a1f29c8f
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-10-18 15:47:20 +0300

    previous entries functionality added

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+let saveElement = document.getElementById('save-el');
+
 let countElement = document.getElementById('count-el');
 let count = 0;
 
 function incrementBtn() {
     console.log('btn was clicked');
-    count = count + 1;
+    count += 1;
     countElement.innerText = count;
 }
 // incrementBtn(); - do not invoke function here!!! it has been already invoked in html code in button!
 
 function save() {
+    let separator = ' - ';
+
+    let saveMessage = count + ' ' + separator + ' ';
+
+    saveElement.textContent += saveMessage;
+
     console.log(count);
 }
