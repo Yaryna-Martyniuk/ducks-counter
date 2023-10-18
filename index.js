583183748a46eb08effffbcc247c544572dce03f
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-10-18 14:46:42 +0300

    save button added

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// document.getElementById('count-el').innerText = 5;
-
+let countElement = document.getElementById('count-el');
 let count = 0;
-
-let countEl = document.getElementById('count-el');
-
-console.log(countEl);
 
 function incrementBtn() {
     console.log('btn was clicked');
-    count++;
-    console.log(count);
-    countEl.innerText = count;
+    count = count + 1;
+    countElement.innerText = count;
 }
-// incrementBtn(); - do not invoke fuction here!!! it has been already invoked in html code in button!
- 
+// incrementBtn(); - do not invoke function here!!! it has been already invoked in html code in button!
+
+function save() {
+    console.log(count);
+}
