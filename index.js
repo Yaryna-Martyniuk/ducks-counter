7b341a706500f7eca3313c0bdf81d07103c3577d
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-10-18 14:27:10 +0300

    button click increments the count

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // document.getElementById('count-el').innerText = 5;
 
-// let count = 0;
+let count = 0;
 
-// console.log(count);
+let countEl = document.getElementById('count-el');
 
-function increment (){
-    console.log("btn was clicked");
+console.log(countEl);
+
+function incrementBtn() {
+    console.log('btn was clicked');
+    count++;
+    console.log(count);
+    countEl.innerText = count;
 }
+// incrementBtn(); - do not invoke fuction here!!! it has been already invoked in html code in button!
+ 
