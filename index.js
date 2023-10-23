7e929e067e771506c56af68e0dd0c2740eb2ccab
Yaryna Martyniuk <113057976+Yaryna-Martyniuk@users.noreply.github.com>
2023-10-23 12:22:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ let countElement = document.getElementById('count-el');
 let count = 0;
 
 function incrementBtn() {
-    console.log('btn was clicked');
+    console.log('btn was clicked'); //to check if the function works 
     count += 1;
-    // countElement.innerText = count;
-    countElement.textContent = count;
+    // countElement.innerText = count; 
+    countElement.textContent = count;  //textContent is an alternative to innerText but renders hidden symbols like spaces etc. Also, innerText triggers the reflow every time new styles appear so it is better to use textContent  instead
 }
-// incrementBtn(); - do not invoke function here!!! it has been already invoked in html code in button!
+// incrementBtn(); - do not invoke function here!!! it has already been invoked in the HTML code in the button tag!
 
 function save() {
     let separator = ' - ';
 
     let saveMessage = count + ' ' + separator + ' ';
 
-    saveElement.textContent += saveMessage; //textContent is an alternative to innerText, but renders hidden symbols like spaces etc.Also innerText trigger the reflow every tiem to check new styles appears so it is better to use textContent  instead
+    saveElement.textContent += saveMessage;
 
     console.log(count);
     count = 0;
